f107d24d5620c88066bfa4310520c137c2be69a4
jaazmeyn <jasmin.blanda@gmail.com>
2021-05-12 22:09:34 +0200

    pass from child to parent onAddGoal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
-import GoalList from './components/GoalList';
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
+
 import './App.css';
 
 const App = () => {
+  const courseGoals = [
+    {id:'cg1', text: 'Finish the Course'}, 
+    {id:'cg2', text: 'earn all about the Course Main Topic'},
+    {id:'cg3', text: 'Help other students in the Course Q&amp;A'}
+];
+
+const addNewGoalHandler = (newGoal) => {
+  courseGoals.push(newGoal);
+  console.log(courseGoals);
+};
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <GoalList />
+      <NewGoal onAddGoal={addNewGoalHandler}/>
+      <GoalList goals={courseGoals}/>
     </div>
   );
 };
