9a298936b1ede8d777b191bcaf7f56530d599530
jaazmeyn <jasmin.blanda@gmail.com>
2021-05-12 22:19:01 +0200

    add use State

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import GoalList from './components/GoalList/GoalList';
 import NewGoal from './components/NewGoal/NewGoal';
@@ -6,15 +6,14 @@ import NewGoal from './components/NewGoal/NewGoal';
 import './App.css';
 
 const App = () => {
-  const courseGoals = [
+  const [courseGoals, setCourseGoals] = useState([
     {id:'cg1', text: 'Finish the Course'}, 
     {id:'cg2', text: 'earn all about the Course Main Topic'},
     {id:'cg3', text: 'Help other students in the Course Q&amp;A'}
-];
+]);
 
 const addNewGoalHandler = (newGoal) => {
-  courseGoals.push(newGoal);
-  console.log(courseGoals);
+  setCourseGoals(courseGoals.concat(newGoal));
 };
   return (
     <div className="course-goals">
